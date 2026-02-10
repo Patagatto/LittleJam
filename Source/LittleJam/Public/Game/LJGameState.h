178f9aadb3d6bb9f2177cba3e16b4b696b0a831a
Nicolas <nicolas.mancone11@gmail.com>
2026-02-10 09:52:49 +0100

    base classes

---
+++ Source/LittleJam/Public/Game/LJGameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "LJGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LITTLEJAM_API ALJGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/LittleJam/Public/Game/LJGamemode.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LJGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LITTLEJAM_API ALJGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
